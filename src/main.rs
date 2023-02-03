e4ab3c94a848c54bc75f0e687dbb6b4d978b8818
Sean <smcgovern135@gmail.com>
2023-02-02 21:57:37 -0500

    basic file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let files = parse_args(&args);
+    let contents = fs::read_to_string(files.file1)
+        .expect("unable to read file");
+}
+struct Files {
+    file1: String,
+    file2: String
+}
+
+impl Files {
+    fn new(args: &[String]) {
+
+    }
+}
+
+fn parse_args(args: &[String]) -> Files {
+    let file1 = args[1].clone();
+    let file2 = args[2].clone();
+    Files { file1, file2 }
 }
